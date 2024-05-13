9a760f04fa56c6554be734af84353c4e7f65d3d4
ninja8750 <72515260+ninja8750@users.noreply.github.com>
2024-05-13 18:56:51 +0000

    changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1443
+let accountEmail = "nk2345@gmail.com"
+var password = "123"
+accountCity= "Delhi"
+let accountState
+// accountId = 2
+
+accountEmail = "asdf"
+password = "2345"
+city="jaipur"
+
+// console.log(accountId)
+console.table([accountId,accountEmail,password,city,accountState])
+
+
+/*
+Prefer not to use var.
+Because it block scope and functional scope
+*/
