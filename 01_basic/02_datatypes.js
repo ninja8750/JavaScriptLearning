3156726aafca0321a110f0d42086321d86434f91
ninja8750 <72515260+ninja8750@users.noreply.github.com>
2024-05-13 18:55:48 +0000

    more changes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"  // trear all JS as newer version
+
+// alert(3+3) //we are using nodjs, not browser
+
+
+let name = "Ninja"
+let age = 25
+let haveRecord = true
+
+// number =>2 to power 53
+// bigint
+// String ==>"/'
+// booleam => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
